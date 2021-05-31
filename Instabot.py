d0fbb9c378d2f903a7c4b852ed5168080470e131
jimchrist-x <jimchrist2@gmail.com>
2021-05-31 16:13:37 +0300

    Adding printing unfollowers function

--- Instabot.py
+++ Instabot.py
@@ -8,9 +8,21 @@ class Instabot:
 		self.bot = Bot()
 		self.bot.login(username=self.username, password=self.password)
 	def print_unfollowers(self):
+		followers = self.bot.get_user_followers(USERNAME)
+		following = self.bot.get_user_following(USERNAME)
+		for elem in following:
+			if elem in followers:
+				pass
+			else:
+				user_name = self.bot.get_username_from_user_id(elem)
+				print(user_name)
+	def print_unfollowed_followers(self):
+		pass
+	def unfollow_unfollowers(self):
 		pass
 	def follow_followers(self):
 		pass
 
 if __name__ == '__main__':
 	instabot = Instabot()
+	instabot.print_unfollowers()
